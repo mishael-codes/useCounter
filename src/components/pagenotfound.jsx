690655345242b24850cebb4cbc273429c91b1e46
mishael-codes <enyimishael22@gmail.com>
2023-10-17 15:52:12 +0100

    added an icon to use in the 404 page

--- src/components/pagenotfound.jsx
+++ src/components/pagenotfound.jsx
@@ -1,9 +1,11 @@
+import unplugged from "../assets/icons/unplugged.png"
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <>
       <div>
+        <img src={unplugged} alt="unplugged wires" />
         <h1 className="leading text-orange-500 text-4xl">Page Not Found</h1>
         <p className="text-2xl">
           It seems like you entered a wrong url. <br /> Don&nbsp;t worry,{" "}
